1cef3b41bbec02815507635b5129b53909f3d17f
Chip Irek <chip.irek@gmail.com>
2016-03-06 11:15:22 -0500

    Migrated footer to new skin + kept tests passing

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,8 @@ Rails.application.routes.draw do
   get     'help'    => 'static_pages#help'
   get     'about'   => 'static_pages#about'
   get     'contact' => 'static_pages#contact'
+  get     'terms' => 'static_pages#terms'
+  get     'privacy' => 'static_pages#privacy'
   get     'news'    => 'static_pages#news'
   get     'signup'  => 'users#new'
   get     'login'   => 'sessions#new'
